767450328cf3660af32e06eb4d6174f4cc6702e9
Fireye <codekai16@gmail.com>
2025-08-10 17:36:41 -0700

    Auto sort todos

--- static/Scripts/about.js
+++ static/Scripts/about.js
@@ -56,16 +56,90 @@ async function loadTODO() {
 		);
 		var list = document.getElementById("todo-list");
 		let vals = await response.json();
+		let uncat = [];
+		let high_prio = [];
+		let mod_prio = [];
+		let low_prio = [];
+		let content = [];
 		let todo_html = "";
 		for (let i = 0; i < vals.length; i++) {
-			todo_html +=
-				'<li><a href="' +
-				vals[i]["html_url"] +
-				'">' +
-				vals[i]["title"] +
-				"</a></li>";
+			if (vals[i]["pull_request"]) {
+				continue;
+			}
+			let labels = [];
+			for (let j = 0; j < vals[i]["labels"].length; j++) {
+				labels.push(vals[i]["labels"][j]["name"]);
+			}
+
+			if (labels.indexOf("content") !== -1) {
+				content.push(vals[i]);
+			} else if (labels.indexOf("High Priority") !== -1) {
+				high_prio.push(vals[i]);
+			} else if (labels.indexOf("Moderate Priority") !== -1) {
+				mod_prio.push(vals[i]);
+			} else if (labels.indexOf("Low Priority") !== -1) {
+				low_prio.push(vals[i]);
+			} else {
+				uncat.push(vals[i]);
+			}
 			// console.log(vals[i]);
 		}
+		todo_html += "<h3>High Priority</h3><ul>";
+
+		for (let i = 0; i < high_prio.length; i++) {
+			todo_html +=
+				'<li><a href="' +
+				high_prio[i]["html_url"] +
+				'">' +
+				high_prio[i]["title"] +
+				"</a></li>";
+		}
+
+		todo_html += "</ul><h3>Moderate Priority</h3><ul>";
+
+		for (let i = 0; i < mod_prio.length; i++) {
+			todo_html +=
+				'<li><a href="' +
+				mod_prio[i]["html_url"] +
+				'">' +
+				mod_prio[i]["title"] +
+				"</a></li>";
+		}
+
+		todo_html += "</ul><h3>Low Priority</h3><ul>";
+
+		for (let i = 0; i < low_prio.length; i++) {
+			todo_html +=
+				'<li><a href="' +
+				low_prio[i]["html_url"] +
+				'">' +
+				low_prio[i]["title"] +
+				"</a></li>";
+		}
+
+		todo_html += "</ul><h3>Content</h3><ul>";
+
+		for (let i = 0; i < content.length; i++) {
+			todo_html +=
+				'<li><a href="' +
+				content[i]["html_url"] +
+				'">' +
+				content[i]["title"] +
+				"</a></li>";
+		}
+
+		todo_html += "</ul><h3>Uncategorized</h3><ul>";
+
+		for (let i = 0; i < uncat.length; i++) {
+			todo_html +=
+				'<li><a href="' +
+				uncat[i]["html_url"] +
+				'">' +
+				uncat[i]["title"] +
+				"</a></li>";
+		}
+		todo_html += "</ul>";
+
 		list.innerHTML = todo_html;
 	} catch (error) {
 		console.error(error.message);
